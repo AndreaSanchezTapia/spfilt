837abc10da176c0670ca365a458ece799269f76a
Andrea Sánchez Tapia <katori@gmail.com>
2019-09-05 23:10:00 -0700

    modified version of filt

--- R/filt.R
+++ R/filt.R
@@ -27,7 +27,7 @@
 #'
 #' @export
 
-filt <- function(pts,
+filt_andrea <- function(pts,
                  inverted = TRUE,
                  shape.municipios = NULL,
                  name.col = c("nome", "stateProvince")) { #poderia ser name.col = c("NOME", "NOMEUF")
@@ -42,15 +42,17 @@ filt <- function(pts,
   }
    if (any(pts[, "lat"] > 90) |
        any(pts[, "lat"] < (-90))) {
-       stop(
+       warning(
            "There is no latitude greater than 90° or less than -90°"
        )
+       pts[,"lat"] <- 0
        }
    if (any(pts[, "lon"] > 90) |
        any(pts[, "lon"] < (-90))) {
-       stop(
+       warnings(
            "There is no latitude greater than 90° or less than -90°"
        )
+       pts[,"lon"] <- 0
        }
 
 
@@ -78,58 +80,40 @@ filt <- function(pts,
 
   pts1 <- pts1 %>% mutate_at(5:ncol(pts1), .funs = function(x) ifelse(!is.na(x), textclean::replace_non_ascii(tolower(x)), x))
 
-  
+
   pts1$filt <- "NA"
 
-  for (i in 1:nrow(pts1)) {
-    if (is.na(pts1$municipality[i])) {
-      pts1[i, "filt"] <- "original municipality not informed"
-    }
-    if(is.na(pts1$municipality[i]) == F) {
-      if (is.na(pts1$municipality[i] == pts1$nome[i]) == TRUE) {
-        pts1[i, "filt"] = "outside Brazil"
-        pts1[i, "nome"] = "outside Brazil"
-        pts1[i, "stateProvince"] = "outside Brazil"
-      }
-      if ((pts1$municipality[i] == pts1$nome[i]) == FALSE) {
-        pts1[i, "filt"] = "outside municipality"
-      }
-      if ((pts1[i, "nome"] == "outside Brazil")) {
-        pts1[i, "filt"] = "outside Brazil"
-        pts1[i, "nome"] = "not found"
-        pts1[i, "stateProvince"] = "not found"
-      }
-    }
-  }
+      pts1 <- pts1 %>%
+          mutate(filt = ifelse(is.na(municipality) | municipality=="", "original municipality not informed", filt)) %>%
+          mutate(filt = ifelse(!is.na(municipality) & !is.na(nome) & municipality != nome, "outside municipality", filt)) %>%
 
-  pts2 <- pts1[, -c(6, 8)]
-  names(pts2) = c("ID",
-                  "species",
-                  "lon",
-                  "lat",
-                  "county.orig" ,
-                  "county.shape",
-                  "status")
+          mutate(filt = ifelse(is.na(municipality == nome), "outside Brazil", filt)) %>%
+          mutate(filt = ifelse(!is.na(municipality) &
+                                   municipality != "" &
+                                   !is.na(nome) & nome != "" & municipality == nome, "OK", filt))
+
+      #guardar esto#pts2 %>% mutate(municipality = ifelse(municipality == "" &
+       #                                         !is.na(nome),
+        #                                    nome,
+         #                                   municipality))
+  pts2 <- pts1 %>% dplyr::select(-adm1, -stateProvince)
+
+  pts2 <- pts2 %>% rename("county.original" = "municipality",
+                         "county.shape" = "nome")
 
   if (inverted == T) {
-    for(i in 1:dim(pts2)[1]){
-      if(pts2$status[i] != "Ok"){
-        valor1 = pts2[i, c("lon", "lat")]
-        coordinates(valor1) = ~lat+lon
-        proj4string(valor1) = proj4string(br_mun)
-        muni = as.vector(over(valor1, br_mun)[, 'nome'])
-        if(is.na(muni)==FALSE){
-          muni <- textclean::replace_non_ascii(muni)
-
-          if(pts2$county.orig[i] == muni){
-            pts2$status[i] = "inverted coordinates"
-          }
-        }
-      }
+    #pts3 <- pts2 %>% filter(filt != "OK")
+    valor1 <- pts2
+    coordinates(valor1) <- ~ lat + lon
+    proj4string(valor1) <- proj4string(br_mun)
+    muni <- over(valor1, br_mun)[, name.col]
+    if (any(!is.na(muni))) {
+    muni2 <- muni %>% mutate_all( .funs = function(x) ifelse(!is.na(x), textclean::replace_non_ascii(tolower(x)), x))
+    pts2 <- pts2 %>% mutate(filt = ifelse(county.original == muni2[,1], "inverted coordinates", filt))
     }
-  }
-
-  print(table(pts2$status))
+          }
+  print(table(pts2$filt))
   cat("\n\n")
   return(pts2)
-}
+
+  }
